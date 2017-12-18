95e1d0b07ac5b82bff1540ba945432845f7216b7
it-svit <dev@itsvit.org>
2017-12-18 18:06:25 +0200

    Fix iam

--- iam.tf
+++ iam.tf
@@ -54,16 +54,13 @@ data "aws_iam_policy_document" "role" {
 }
 
 module "role_label" {
-  source    = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.2.1"
-  namespace = "${var.namespace}"
-  stage     = "${var.stage}"
-  name      = "${var.name}"
-  delimiter = "${var.delimiter}"
-
-  attributes = ["${compact(concat(var.attributes, list("partial-role")))}"]
-
-  # attributes = ["${compact(concat(var.attributes, list("resource"), list("role")))}"]
-  tags = "${var.tags}"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.2.1"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${var.name}"
+  delimiter  = "${var.delimiter}"
+  attributes = ["${compact(concat(var.attributes, list("role")))}"]
+  tags       = "${var.tags}"
 }
 
 resource "aws_iam_role" "role" {
